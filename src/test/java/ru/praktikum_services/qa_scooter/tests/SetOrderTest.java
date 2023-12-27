17e432e88a19fd651a5518f31d832ae009ecb133
Anton Solodovnikov <kivifruit1@yandex.ru>
2023-12-27 21:49:14 +0300

    add tests for sprint4

--- src/test/java/ru/praktikum_services/qa_scooter/tests/SetOrderTest.java
+++ src/test/java/ru/praktikum_services/qa_scooter/tests/SetOrderTest.java
@@ -6,6 +6,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import ru.praktikum_services.qa_scooter.page_objects.MainPage;
 import ru.praktikum_services.qa_scooter.page_objects.OrderPage;
 import ru.praktikum_services.qa_scooter.page_objects.RentPage;
